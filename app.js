9685b238109d4329e7d52c0698109d7303933e9b
murri <ruslanliakhovets@outlook.com>
2021-12-16 22:58:00 +0200

    1.3.1 fixed console write

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const countdown = () => {
     const textHour = Math.floor((gap % day) / hour);
     const textMinute = Math.floor((gap % hour) / minute);
     const textSecond = Math.floor((gap % minute) / second);
-    console.log(textDay, textHour, textMinute, textSecond)
+
     document.querySelector('.day').innerText = textDay;
     document.querySelector('.hour').innerText = textHour;
     document.querySelector('.minute').innerText = textMinute;
