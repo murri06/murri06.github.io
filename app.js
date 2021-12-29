8e56e8264170887a6f69945f1c6c4a509b21a608
murri <ruslanliakhovets@outlook.com>
2021-12-29 13:17:16 +0200

    2.0 changed js script, stopping script at the end of countdown, added present on button while the countdown is ended

--- app.js
+++ app.js
@@ -13,29 +13,28 @@ const countdown = () => {
     const textMinute = Math.floor((gap % hour) / minute);
     const textSecond = Math.floor((gap % minute) / second);
 
-    document.querySelector('.day').innerText = textDay;
-    document.querySelector('.hour').innerText = textHour;
-    document.querySelector('.minute').innerText = textMinute;
-    document.querySelector('.second').innerText = textSecond;
+    if (gap >= 0) {
+        document.querySelector('.day').innerText = textDay;
+        document.querySelector('.hour').innerText = textHour;
+        document.querySelector('.minute').innerText = textMinute;
+        document.querySelector('.second').innerText = textSecond;
+    }
     
     if (textDay < 1) {
         document.querySelector('#already').innerText = "Відкрий мене!";
         document.querySelector('#already').disabled = false;
     }
+    if (textDay === 1) {
+        document.querySelector('.dnes').innerText = "День";
+    }
+    if (textDay === 2 || textDay === 3 || textDay === 4) {
+        document.querySelector('.dnes').innerText = "Дні";
+    } else
+        document.querySelector('.dnes').innerText = "Днів";
 };
 
 setInterval(countdown, 1000);
 
-// var alert1 = document.getElementById('alertdialog');
-// var ready = document.getElementById('already');
-
 function present() {
-    window.open();
+    window.open('https://sale.karabas.com/b5f556e4256cc27ab4137b41e9f1094a3373da71ab8f63473b56ad2dd246393c.ticket');
 }
-// ready.addEventListener('click', function onOpen() {
-//     if (typeof alert1.showModal === "function") {
-//         alert1.showModal();
-//     } else {
-//         alert("The <dialog> API is not supported by this browser");
-//     }
-// });
